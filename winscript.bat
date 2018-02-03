e6cae6f672397175886f0ca5ac8b1a4eac85afb8
sambireddy26 <sambireddy26@gmail.com>
2018-02-03 18:26:19 +0530

    Add files via upload

---
+++ winscript.bat
@@ -0,0 +1,18 @@
+@echo off
+
+
+
+setlocal ENABLEDELAYEDEXPANSION
+
+
+set FNAME=%1
+set SNAME=%2
+set LNAME=%3
+
+
+echo first var:%FNAME% 
+echo 2nd var:%SNAME% 
+echo 3rd var:%LNAME%
+
+echo full name is %fname% %sname% %lname%
+
